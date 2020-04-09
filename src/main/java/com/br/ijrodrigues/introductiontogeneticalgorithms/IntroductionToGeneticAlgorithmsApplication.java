e639cd7298eb3d65172882d0053b2037b1b4f4be
Israel Rodrigues <israel.jesus@tqi.com.br>
2020-04-09 11:07:03 -0300

    Removing some useless things

--- src/main/java/com/br/ijrodrigues/introductiontogeneticalgorithms/IntroductionToGeneticAlgorithmsApplication.java
+++ src/main/java/com/br/ijrodrigues/introductiontogeneticalgorithms/IntroductionToGeneticAlgorithmsApplication.java
@@ -1,13 +1,20 @@
 package com.br.ijrodrigues.introductiontogeneticalgorithms;
 
+import com.br.ijrodrigues.introductiontogeneticalgorithms.domain.Population;
+import com.br.ijrodrigues.introductiontogeneticalgorithms.domain.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+import java.util.ArrayList;
+
+//@SpringBootApplication
 public class IntroductionToGeneticAlgorithmsApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(IntroductionToGeneticAlgorithmsApplication.class, args);
-    }
+//        SpringApplication.run(IntroductionToGeneticAlgorithmsApplication.class, args);
 
+        ArrayList<Product> products = Product.getSomeProducts();
+        Population population = new Population(20, products, 3.0);
+        population.run(500, 1);
+    }
 }
